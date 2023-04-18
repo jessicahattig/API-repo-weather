286450580e7741218945f84ae383e0f783fae0a1
jessicahattig <jessicahattig@gmail.com>
2023-04-18 10:22:19 -0700

    Adding BI, UI logic and new js file.

--- src/index.js
+++ src/index.js
@@ -5,27 +5,39 @@ import './css/styles.css';
 // Business Logic
 
 function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
+  let promise = new Promise(function(resolve,reject){
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
 
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city);
-    }
+    request.addEventListener("loadend", function() {
+      const response = JSON.parse(this.responseText);
+      if (this.status === 200) {
+        resolve([response, city]);
+      } else {
+        reject([this, response, city]);
+      }
+    });
+    request.open("GET", url, true);
+    request.send();
+  });
+  
+  promise.then(function(response){
+    printElements(response);
+  }, function(errorMessage){
+    printError(errorMessage);
   });
 
-  request.open("GET", url, true);
-  request.send();
 }
 
 // UI Logic
 
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%.
-  The temperature in Fahernheit is ${apiResponse.main.temp} degrees. The current wind speed is ${apiResponse.wind.speed} mph. Current forecast is ${apiResponse.weather[0].main}.`;
+function printElements(results) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${results[1]} is ${results[0].main.humidity}%.
+  The temperature in Kelvins is ${results[0].main.temp} degrees.`;
+}
+
+function printError(error) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
 }
 
 function handleFormSubmission(event) {
@@ -34,11 +46,6 @@ function handleFormSubmission(event) {
   document.querySelector('#location').value = null;
   getWeather(city);
 }
-
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
-}
-
 
 window.addEventListener("load", function() {
   document.querySelector('form').addEventListener("submit", handleFormSubmission);
